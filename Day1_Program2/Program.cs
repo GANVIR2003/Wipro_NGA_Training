6d253f9e87b831ed63294319fe9e8ca59c338187
Chhavi Ganvir <ganvirchhavi@gmail.com>
2026-01-14 22:13:33 +0530

    Add Day1_Program file

---
+++ Day1_Program2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//All these using directives are added by default in a new console application project.
+//Framework class library references.
+
+namespace Demo1_HellowWorld
+// Namespace declaration is used for organizing code and preventing name conflicts.
+{
+    internal class Program
+    // Class declaration. 'internal' means it is accessible only within its own assembly.
+    // in C# we have following visibility Scope: 
+    // public, private, protected, internal, protected internal, private protected
+
+    {
+        static void Main(string[] args)
+        {
+            // Main method: Entry point of the application.
+            Console.WriteLine("Hello, World!");
+            // Console.WriteLine is used to print text to the console.
+        }
+    }
+}
+
